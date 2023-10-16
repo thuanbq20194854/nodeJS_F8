1abd7fce5acaff438b86d2b20edd189bb6bbc177
Tarzan.Bui <tarzan.bui@avepoint.com>
2023-10-16 21:50:59 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+
+// Register `hbs.engine` with the Express app.
+
+const app = express();
+const port = 3000;
+
+const route = require("./routes");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// HTTP Logger
+app.use(morgan("combined"));
+
+//Template engine
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.set("views", path.join(__dirname, "resources\\views"));
+
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+const newsRouter = require("./news");
+const siteRouter = require("./site");
+
+function route(app) {
+  app.use("/news", newsRouter);
+  app.use("/", siteRouter);
+
+  app.get("/", (req, res) => {
+    res.render("home");
+  });
+
+  // app.get("/news", (req, res) => {
+  //   res.render("news");
+  // });
+
+  app.get("/search", (req, res) => {
+    res.render("search");
+    console.log(req.body);
+  });
+  app.post("/search", (req, res) => {
+    res.render("search");
+
+    console.log(" req.body: ", req.body);
+  });
+}
+
+module.exports = route;
